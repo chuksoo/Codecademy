29de59c5043f7806a459c185135b9da0e8ff9cc4
chuksoo <mexx4me@yahoo.com>
2022-07-08 22:55:34 -0800

    Updated OOP for Student grades project

--- Projects/Computer Science/CS101-Introduction to Programming/15. Classes/Student grades.py
+++ Projects/Computer Science/CS101-Introduction to Programming/15. Classes/Student grades.py
@@ -5,21 +5,6 @@ Created on Mon Jul  4 21:30:47 2022
 @author: Chukwuemeka Okoli
 """
 
-# define class `Student`
-class Student:
-    # add a constructor for `Student`
-    def __init__(self, name, year):
-        self.name = name
-        self.year = year
-        self.grades = []
-    
-    # add_grade() method
-    def add_grade(self, grade):
-        if type(grade) is Grade:
-            self.grades.append(grade)
-        else:
-            print('Do nothing')
-
 # create a `Grade` class
 class Grade:
     minimum_passing = 65
@@ -27,9 +12,36 @@ class Grade:
     # give `Grade` a constructor
     def __init__(self, score):
         self.score = score
+    def is_passing(self):
+        if self.score >= Grade.minimum_passing:
+            return True
+        return False     
 
-# create instance of class student
-roger = Student("Roger van der Weyden", 10)
-sandro = Student("Sandro Botticelli", 12)
-pieter = Student("Pieter Bruegel the Elder", 8)
-pieter.add_grade(Grade(100))
+# define class `Student`
+class Student:
+    # add a constructor for `Student`
+    def __init__(self, name, year):
+ 	    self.name = name
+ 	    self.year = year
+ 	    self.grades = []
+         
+    # add_grade() method
+    def add_grade(self, grade):
+        if type(grade) == Grade:
+            self.grades.append(grade)
+        else:
+            return
+        
+    # add get_average() method
+    def get_average(self):
+        s = 0
+        for student in self.grades:
+            s += student.score
+        return s/len(self.grades)
+    
+# create instance of class student    
+roger = Student("Roger van der Weyden",10)    
+sandro = Student("Sandro Botticelli",12)
+pieter = Student("Pieter Bruegel the Elder",8)
+x = Grade(65)    
+print(pieter.add_grade(x))
