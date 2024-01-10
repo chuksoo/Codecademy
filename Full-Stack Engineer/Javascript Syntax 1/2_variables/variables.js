e96cce8003da7825f0d342d22e34692b0a7a93f3
chuksoo <mexx4me@yahoo.com>
2024-01-10 01:52:49 -0900

    Completed Variables and Conditionals

---
+++ Full-Stack Engineer/Javascript Syntax 1/2_variables/variables.js
@@ -0,0 +1,66 @@
+// Create a Variable: var (using pre-ES6 version of JavaScript)
+var favoriteFood = 'pizza';
+console.log(favoriteFood);
+
+var numOfSlices = 8;
+console.log(numOfSlices);
+
+// Create a Variable: let (using post-ES6 version of JavaScript)
+let changeMe = true;
+changeMe = false;
+console.log(changeMe)
+
+// Create a Variable: const
+const entree = 'Enchiladas';
+console.log(entree);
+
+// Mathematical Assignment Operators
+let levelUp = 10;
+let powerLevel = 9001;
+let multiplyMe = 32;
+let quarterMe = 1152;
+
+// Use the mathematical assignments in the space below:
+levelUp += 5;
+powerLevel -= 100;
+multiplyMe *= 11;
+quarterMe /= 4;
+
+// These console.log() statements below will help you check the values of the variables.
+// You do not need to edit these statements. 
+console.log('The value of levelUp:', levelUp); 
+console.log('The value of powerLevel:', powerLevel); 
+console.log('The value of multiplyMe:', multiplyMe); 
+console.log('The value of quarterMe:', quarterMe);
+
+// The Increment operator (++) and Decrement operator (--)
+let gainedDollar = 3;
+let lostDollar = 50;
+
+gainedDollar ++;
+console.log(gainedDollar);
+
+lostDollar --;
+console.log(lostDollar)
+
+// String Concatenation with Variables
+let favoriteAnimal = 'dog';
+console.log('My favorite animal: ' + favoriteAnimal);
+
+// String Interpolation
+let myName = 'Chuks';
+let myCity = 'Barcelona';
+console.log(`My name is ${myName}. My favorite city is ${myCity}`);
+
+// typeof operator
+let newVariable = 'Playing around with typeof.';
+console.log(typeof newVariable);
+newVariable = 1
+console.log(typeof newVariable);
+
+
+
+
+
+
+
---
+++ Full-Stack Engineer/Javascript Syntax 1/3_conditionals/conditionals.js
@@ -0,0 +1,163 @@
+// If Statement 
+let sale = true;
+sale = false;
+
+if (sale) {
+  console.log('Time to buy!')
+}
+
+// If...Else Statements 
+sale = false;
+  
+if(sale) {
+    console.log('Time to buy!');
+} else {
+    console.log('Time to wait for a sale.');
+}
+  
+// Comparison Operators
+let hungerLevel = 7;
+
+if (hungerLevel > 7) {
+  console.log('Time to eat!');
+} else {
+  console.log('We can eat later!');
+}
+
+// Logical Operators
+let mood = 'sleepy';
+let tirednessLevel = 6;
+
+if (mood === 'sleepy' && tirednessLevel > 8) {
+  console.log('time to sleep');
+} else {
+  console.log('not bed time yet')
+}
+
+// Truthy and Falsy
+let wordCount = 0;
+if (wordCount) {
+  console.log("Great! You've started your work!");
+} else {
+  console.log('Better get to work!');
+}
+
+let favoritePhrase = 'Hello World!';
+if (favoritePhrase) {
+  console.log("This string doesn't seem to be empty.");
+} else {
+  console.log('This string is definitely empty.');
+}
+
+let newWordCount = 12;
+if (newWordCount) {
+  console.log("Great! You've started your work!");
+} else {
+  console.log('Better get to work!');
+}
+
+let myFavoritePhrase = 'Hello World!';
+if (myFavoritePhrase) {
+  console.log("This string doesn't seem to be empty.");
+} else {
+  console.log('This string is definitely empty.');
+}
+
+// Truthy and Falsy Assignment
+let tool = '';
+tool = 'marker';
+// Use short circuit evaluation to assign writingUtensil variable below:
+let writingUtensil = tool || 'pen';
+
+console.log(`The ${writingUtensil} is mightier than the sword.`);
+
+// Ternary Operator
+let isNightTime = true;
+
+if (isNightTime) {
+  console.log('Turn on the lights!');
+} else {
+  console.log('Turn off the lights!');
+}
+
+isNightTime ? console.log('Turn on the lights!') : console.log('Turn off the lights!'); // Similar to the if...else code above
+
+
+let isLocked = false;
+isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
+
+let isCorrect = true;
+isCorrect ? console.log('Correct!') :console.log('Incorrect!');
+
+let favoritePhrases = 'Love That!';
+favoritePhrases === 'Love That!' ? console.log('I love that!') : console.log("I don't love that!");
+
+// Else If Statements
+let stopLight = 'yellow';
+if (stopLight === 'red') {
+  console.log('Stop!');
+} else if (stopLight === 'yellow') {
+  console.log('Slow down.');
+} else if (stopLight === 'green') {
+  console.log('Go!');
+} else {
+  console.log('Caution, unknown!');
+}
+
+
+let season = 'summer';
+if (season === 'spring') {
+  console.log('It\'s spring! The trees are budding!');
+} else if (season === 'winter') {
+  console.log('It\'s winter! Everything is covered in snow.');
+} else if (season === 'fall') {
+  console.log('It\'s fall! Leaves are falling!');
+} else if (season === 'summer') {
+  console.log('It\'s sunny and warm because it\'s summer!');
+} else {
+  console.log('Invalid season.');
+}
+
+// The switch keyword
+let groceryItem = 'papaya';
+
+if (groceryItem === 'tomato') {
+  console.log('Tomatoes are $0.49');
+} else if (groceryItem === 'papaya'){
+  console.log('Papayas are $1.29');
+} else {
+  console.log('Invalid item');
+}
+
+switch (groceryItem) { // switch statement that prints 'Papayas are $1.29'
+  case 'tomato':
+    console.log('Tomatoes are $0.49');
+    break;
+  case 'lime':
+    console.log('Limes are $1.49');
+    break;
+  case 'papaya':
+    console.log('Papayas are $1.29');
+    break;
+  default:
+    console.log('Invalid item');
+    break;
+}
+
+// Another switch statement
+let athleteFinalPosition = 'first place';
+switch (athleteFinalPosition) {
+  case 'first place':
+    console.log('You get the gold medal!');
+    break;
+  case 'second place':
+    console.log('You get the silver medal!');
+    break;
+  case 'third place':
+    console.log('You get the bronze medal!');
+    break;
+  default:
+    console.log('No medal awarded.');
+    break;
+}
+
